a93cfdc9cb6bbac8d85f125beb41aa65bdf9842d
Your Name <you@example.com>
2025-11-17 13:04:17 +0100

    Zadania z przedmiotu "Podstawy Programowania w python"

---
+++ zad_2.py
@@ -0,0 +1,9 @@
+# zad_2.py
+def multiply_numbers(a: int, b: int) -> int:
+    """Zwraca wynik mnożenia dwóch liczb całkowitych."""
+    return a * b
+
+# Przykład użycia
+print(multiply_numbers(7, 8))
+
+# Wynik: 56
