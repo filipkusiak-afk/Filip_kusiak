c8f48f64ac18504eca53f7c4848dcee0c5fc8e9b
Your Name <you@example.com>
2025-11-24 12:45:42 +0100

    Revert "Zadania z przedmiotu "Podstawy Programowania w python""
    This reverts commit a93cfdc9cb6bbac8d85f125beb41aa65bdf9842d.

--- zad_2.py
+++
@@ -1,9 +0,0 @@
-# zad_2.py
-def multiply_numbers(a: int, b: int) -> int:
-    """Zwraca wynik mnożenia dwóch liczb całkowitych."""
-    return a * b
-
-# Przykład użycia
-print(multiply_numbers(7, 8))
-
-# Wynik: 56
--- zad_4.py
+++
@@ -1,8 +0,0 @@
-def two_arguments_bigger_than_third (a:int, b: int, c: int) -> bool:
-    "sprawdza czy suma a i b jest większa od c"
-    return (a+b) >=c
-
-"Przykłady użycia"
-print(f"3 + 5 >= 7? {two_arguments_bigger_than_third(3, 5, 7)}") #True
-print(f"2 + 4 >= 7? {two_arguments_bigger_than_third(2,4,7)}") #False
-
